f9ecef6f559b3cec9eb4dbe0f5f7240d2931d7d0
Incedendo <nguyenminhkiet94@gmail.com>
2017-10-17 16:32:55 -0500

    Add files via upload

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,25 +30,28 @@ class Home extends Component {
             Welcome to Our Senior Design Project Prototype
         </h1>
 
-        <button onClick={this.displayState}>Display State</button>
+        {/* <button onClick={this.displayState}>Display State</button> */}
+        <p>
+          <h1>Fulcrum</h1>
+            [Explanation for Fulcrum approach]
+          <h1>TBD</h1>
+          [Explanation for TBD approach]
+        </p>
 
         <Grid>
           <Col sm={4} md={6}>
             <h4>Falcrum Approach</h4>
             <input className="tgl tgl-flat" id="cb1" type="checkbox" onClick={this.toggleFalcrum}/>
-            <label className="tgl-btn" htmlFor="cb1"></label>
+            <label className="tgl-btn centeredPosition" htmlFor="cb1"></label>
           </Col>
           <Col sm={4} md={6}>
             <h4>TDB</h4>
-            <input className="tgl tgl-flat" id="cb2" type="checkbox" onClick={this.toggleTBD}/>
-            <label className="tgl-btn" htmlFor="cb2"></label>
+            <input className="tgl tgl-flat" id="cb2 " type="checkbox" onClick={this.toggleTBD}/>
+            <label className="tgl-btn centeredPosition" htmlFor="cb2"></label>
           </Col>
         </Grid>
 
-        {(this.state.falcrumApproach || this.state.tbd) &&
-        <button onClick={this.displayState}>Next</button>}
-
-        <a class="btn-5" href="#">Slice</a>
+        {(this.state.falcrumApproach || this.state.tbd) && <a class="btn-5" href="#" onClick={this.displayState}>Next (Slide Effect)</a>}
       </div>
     )
   }
